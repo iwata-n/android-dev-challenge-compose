215b6c7759830d5f294aec8c77cc16d2199ae5ad
iwata-n <a.tokai.wain@gmail.com>
2021-02-28 01:13:18 +0900

    fix lint

--- app/src/main/java/com/example/androiddevchallenge/AnimalListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/AnimalListScreen.kt
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
@@ -23,7 +42,7 @@ import dev.chrisbanes.accompanist.coil.CoilImage
 @Composable
 fun AnimalListScreen(animalRepository: AnimalRepository, navController: NavController) {
     Scaffold(
-        topBar = { TopAppBar(title = {Text("Animal List")}) },
+        topBar = { TopAppBar(title = { Text("Animal List") }) },
     ) {
         AnimalList(
             animals = animalRepository.all(),
